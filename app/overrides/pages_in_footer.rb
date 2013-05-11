16d454260aa1ab27d9071d9e9eecb475ab97cc73
psymeon <petros.symeonidis@gmail.com>
2013-05-11 19:47:05 +0300

    Update pages_in_footer.rb

--- app/overrides/pages_in_footer.rb
+++ app/overrides/pages_in_footer.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
                      :name => "pages_in_footer",
-                     :insert_bottom => "#footer-right",
+                     :insert_top => "#footer_pages",
                      :partial => "spree/static_content/static_content_footer",
                      :disabled => false)
